c373279d9c96ffcd0cd960d90ea1ac048ffed2c1
gxyau <GuoXian.Yau@gmail.com>
2020-09-05 14:56:22 +0200

    Sockets, styling

--- server.cpp
+++ server.cpp
@@ -6,8 +6,8 @@
 #include <netinet/in.h> 
 #include <string.h> 
 #define PORT 8080 
-int main(int argc, char const *argv[]) 
-{ 
+
+int main(int argc, char const *argv[]) { 
 	int server_fd, new_socket, valread; 
 	struct sockaddr_in address; 
 	int opt = 1; 
@@ -16,16 +16,14 @@ int main(int argc, char const *argv[])
 	char *hello = "Hello from server"; 
 	
 	// Creating socket file descriptor 
-	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
-	{ 
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) { 
 		perror("socket failed"); 
 		exit(EXIT_FAILURE); 
 	} 
 	
 	// Forcefully attaching socket to the port 8080 
 	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, 
-												&opt, sizeof(opt))) 
-	{ 
+												&opt, sizeof(opt))) { 
 		perror("setsockopt"); 
 		exit(EXIT_FAILURE); 
 	} 
@@ -35,19 +33,16 @@ int main(int argc, char const *argv[])
 	
 	// Forcefully attaching socket to the port 8080 
 	if (bind(server_fd, (struct sockaddr *)&address, 
-								sizeof(address))<0) 
-	{ 
+								sizeof(address))<0) { 
 		perror("bind failed"); 
 		exit(EXIT_FAILURE); 
 	} 
-	if (listen(server_fd, 3) < 0) 
-	{ 
+	if (listen(server_fd, 3) < 0) { 
 		perror("listen"); 
 		exit(EXIT_FAILURE); 
 	} 
 	if ((new_socket = accept(server_fd, (struct sockaddr *)&address, 
-					(socklen_t*)&addrlen))<0) 
-	{ 
+					(socklen_t*)&addrlen))<0) { 
 		perror("accept"); 
 		exit(EXIT_FAILURE); 
 	} 
