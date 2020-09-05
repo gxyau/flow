5c9bb4701928320cf6b543329723eeaebd234f2c
gxyau <GuoXian.Yau@gmail.com>
2020-09-05 23:40:10 +0200

    Basic working model, needs debugging

--- client.cpp
+++ client.cpp
@@ -4,12 +4,18 @@
 #include <arpa/inet.h> 
 #include <unistd.h> 
 #include <string.h> 
+#include <cstring>
+#include <iostream>
+#include "message_specs.h"
 #define PORT 8080 
+using std::cin;
+using std::cout;
+using std::endl;
 
 int main(int argc, char const *argv[]) { 
 	int sock = 0, valread; 
 	struct sockaddr_in serv_addr; 
-	char *hello = "Hello from client"; 
+	//char *hello = "Hello from client"; 
 	char buffer[1024] = {0}; 
 	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) { 
 		printf("\n Socket creation error \n"); 
@@ -28,8 +34,94 @@ int main(int argc, char const *argv[]) {
 	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) { 
 		printf("\nConnection Failed \n"); 
 		return -1; 
-	} 
-	send(sock , hello , strlen(hello) , 0 ); 
+	}
+    // Get message
+    /*
+    Header header;
+    NewOrder newOrder;
+    DeleteOrder deleteOrder;
+    ModifyOrderQuantity modifyOrderQuantity;
+    uint16_t messageType;
+    uint64_t listingId, orderId, orderQuantity, orderPrice, newQuantity;
+    char side, *message;
+    cout << "Insert message type: ";
+    cin  >> messageType;
+    switch (messageType) {
+        case 1: cin >> listingId;
+                cin >> orderId;
+                cin >> orderQuantity;
+                cin >> orderPrice;
+                cin >> side;
+                newOrder.messageType   = 1;
+                newOrder.listingId     = listingId;
+                newOrder.orderId       = orderId;
+                newOrder.orderQuantity = orderQuantity;
+                newOrder.orderPrice    = orderPrice;
+                newOrder.side          = side;
+                // Define header
+                header.version        = 0;
+                header.payloadSize    = 35;
+                header.sequenceNumber = 0;
+                header.timestamp      = 1;
+                // Defining message
+                message = new char[51];
+                std::memcpy(message, header, 16);
+                std::memcpy(message+16, newOrder, 35);
+                break;
+        case 2: uint64_t orderId;
+                cin >> orderId;
+                deleteOrder.messageType = 2;
+                deleteOrder.orderId     = orderId;
+                // Define header
+                header.version        = 0;
+                header.payloadSize    = 10;
+                header.sequenceNumber = 0;
+                header.timestamp      = 1;
+                // Defining message
+                message = new char[26];
+                std::memcpy(message, header, 16);
+                std::memcpy(message+16, newOrder, 10);
+                break;
+        case 3: uint64_t orderId, newQuantity;
+                cin >> orderId;
+                cin >> newQuantity;
+                modifyOrderQuantity.messageType = 3;
+                modifyOrderQuantity.orderId     = orderId;
+                modifyOrderQuantity.newQuantity = newQuantity;
+                // Define header
+                message = new char[34];
+                header.version        = 0;
+                header.payloadSize    = 18;
+                header.sequenceNumber = 0;
+                header.timestamp      = 1;
+                // Defining message
+                std::memcpy(message, header, 16);
+                std::memcpy(message+16, newOrder, 18);
+                break;
+        default: cout << "Invalid message type";
+                 break;
+    }
+    */
+    Header header;
+    header.version        = 0;
+    header.payloadSize    = 35;
+    header.sequenceNumber = 0;
+    header.timestamp      = 1;
+    NewOrder payload;
+    payload.messageType   = 1;
+    payload.listingId     = 1;
+    payload.orderId       = 1;
+    payload.orderQuantity = 10;
+    payload.orderPrice    = 10;
+    payload.side          = 'B';
+    char *message = new char[51];
+    std::memcpy(message, &header, 16);
+    std::memcpy(message+16, &payload, 35);
+    // Sending message
+    for (int i = 0; i < 51; ++i) cout << std::hex << (int) message[i];
+    cout << endl;
+    send(sock, message, 51, 0);
+	//send(sock , hello , strlen(hello) , 0 ); 
 	printf("Hello message sent\n"); 
 	valread = read( sock , buffer, 1024); 
 	printf("%s\n",buffer ); 
--- server.cpp
+++ server.cpp
@@ -5,7 +5,83 @@
 #include <stdlib.h> 
 #include <netinet/in.h> 
 #include <string.h> 
+#include <cstring>
+#include <iostream>
+#include <unordered_map>
+#include "message_specs.h"
 #define PORT 8080 
+using std::unordered_map;
+
+// Threshold
+int BUY_THRESHOLD = 20, SELL_THRESHOLD = 15;
+
+
+struct Order {
+        int orderId, financialInstrumentId, qty, price;
+        char side;
+        Order() : orderId(), financialInstrumentId(), qty(), price(), side() {}
+        Order(int oId, int fiId, int q, int p, char s) : orderId(oId), financialInstrumentId(fiId), qty(q), price(p), side(s) {}
+};
+
+// Map of orders
+unordered_map<int, Order*> orders;
+
+class FinancialInstrument {
+    public:
+        int netPos = 0, buyQty = 0, sellQty = 0;
+        
+        int getHypotheticalBuy() const { return std::max(buyQty, netPos + buyQty);
+        }
+
+        int getHypotheticalSell() const {
+            return std::max(sellQty, sellQty - netPos);
+        }
+
+        bool addOrder(Order &order) {
+            switch (order.side) {
+                case 'B': if (order.qty + buyQty > BUY_THRESHOLD) return false;
+                        buyQty += order.qty;    
+                        break;
+                case 'S': if (order.qty + sellQty > SELL_THRESHOLD) return false;
+                        sellQty += order.qty;
+                        break;
+            }
+            orders[order.orderId] = &order;
+            return true;
+        }
+
+        bool modifyOrder(Order &order, int newQty) {
+            switch (order.side) {
+                case 'B': if (newQty - order.qty + buyQty > BUY_THRESHOLD) return false;
+                        buyQty   += newQty - order.qty;
+                        order.qty = newQty;
+                        break;
+                case 'S': if (newQty - order.qty + sellQty > SELL_THRESHOLD) return false;
+                        sellQty   += newQty - order.qty;
+                        order.qty  = newQty;
+                        break;
+            }
+            return true;
+        }
+
+        void deleteOrder(Order &order) {
+            auto it = orders.find(order.orderId);
+            if (it != orders.end()) {
+                switch(order.side) {
+                    case 'B': buyQty  -= order.qty;
+                    case 'S': sellQty -= order.qty;
+                }
+                orders.erase(it);
+            }
+        }
+
+        void trade(int tradeQty, char side) {
+            netPos += tradeQty * (side = 'B' ? 1 : -1);
+        }
+};
+
+// Map of financial instruments
+unordered_map<int, FinancialInstrument*> financialInstruments;
 
 int main(int argc, char const *argv[]) { 
 	int server_fd, new_socket, valread; 
@@ -13,7 +89,7 @@ int main(int argc, char const *argv[]) {
 	int opt = 1; 
 	int addrlen = sizeof(address); 
 	char buffer[1024] = {0}; 
-	char *hello = "Hello from server"; 
+	//char *hello = "Hello from server"; 
 	
 	// Creating socket file descriptor 
 	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) { 
@@ -46,9 +122,56 @@ int main(int argc, char const *argv[]) {
 		perror("accept"); 
 		exit(EXIT_FAILURE); 
 	} 
-	valread = read( new_socket , buffer, 1024); 
+    // Read header
+    Header header;
+	valread = read( new_socket , buffer, 16); 
+    std::memcpy(&header, buffer, 16);
+    valread = read(new_socket, buffer, header.payloadSize);
+    // Get message type
+    uint16_t *messageType = (uint16_t*) buffer;
+    // Initiate order
+    Order order;
+    switch(*messageType) {
+        case 1: { 
+                    NewOrder newOrder;
+                    std::memcpy(&newOrder, buffer, header.payloadSize);
+                    order = Order(newOrder.orderId, newOrder.listingId, newOrder.orderQuantity, newOrder.orderPrice, newOrder.side);
+                    if (!financialInstruments.count(newOrder.listingId)) {
+                        FinancialInstrument *fi = new FinancialInstrument();
+                        financialInstruments[newOrder.listingId] = fi;
+                    }
+                    financialInstruments[newOrder.listingId]->addOrder(order);
+                    break; 
+                }
+        case 2: { 
+                    DeleteOrder deleteOrder;
+                    std::memcpy(&deleteOrder, buffer, header.payloadSize);
+                    order = *orders.find(deleteOrder.orderId)->second;
+                    FinancialInstrument fi = *financialInstruments.find(order.financialInstrumentId)->second;
+                    fi.deleteOrder(order);
+                    break; 
+                }
+        case 3: { 
+                    ModifyOrderQuantity modifyOrderQuantity;
+                    std::memcpy(&modifyOrderQuantity, buffer, header.payloadSize);
+                    order = *orders.find(modifyOrderQuantity.orderId)->second;
+                    FinancialInstrument fi = *financialInstruments.find(order.financialInstrumentId)->second;
+                    fi.modifyOrder(order, modifyOrderQuantity.newQuantity);
+                    break; 
+                }
+        case 4: {
+                    Trade trade;
+                    std::memcpy(&trade, buffer, header.payloadSize);
+                    order = *orders.find(trade.tradeId)->second;
+                    FinancialInstrument fi = *financialInstruments.find(trade.listingId)->second;
+                    fi.trade(trade.tradeQuantity, order.side);
+                    break;
+                }
+    }
+    std::cout << "Financial instruments is empty: " << financialInstruments.empty() << std::endl;
+    std::cout << "Orders is empty: " << orders.empty() << std::endl;
 	printf("%s\n",buffer ); 
-	send(new_socket , hello , strlen(hello) , 0 ); 
+	//send(new_socket , hello , strlen(hello) , 0 ); 
 	printf("Hello message sent\n"); 
 	return 0; 
 } 
